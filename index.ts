d7a0c7acdc9836295a82875c46ca8d2db2e87503
Josh Lee <josh@joshbe.me>
2023-09-12 00:37:01 +0100

    run scraper straight away

--- index.ts
+++ index.ts
@@ -4,6 +4,9 @@ import * as schedule from "node-schedule"
 // Start express server
 const express = fork('express');
 
+// Run scraper straight away
+fork('scrape_instagram_profiles');
+
 const scraper = schedule.scheduleJob('0 20 * * *', () => {
     fork('scrape_instagram_profiles')
 })
