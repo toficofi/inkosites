7d239987d2a36ae982e6f57017ba1ec22a130d59
Josh Lee <josh@joshbe.me>
2023-09-12 00:35:46 +0100

    cron every day

--- index.ts
+++ index.ts
@@ -4,6 +4,6 @@ import * as schedule from "node-schedule"
 // Start express server
 const express = fork('express');
 
-const scraper = schedule.scheduleJob('* * * * *', () => {
+const scraper = schedule.scheduleJob('0 20 * * *', () => {
     fork('scrape_instagram_profiles')
 })
